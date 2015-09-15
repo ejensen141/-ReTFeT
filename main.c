7ba3335371056f5bb8fce8e0e567658af6c6fc55
Geoffrey Lentner <glentner@nd.edu>
2015-09-15 10:47:10 -0400

    (c) statement, MIT

--- main.c
+++ main.c
@@ -1,3 +1,10 @@
+/*
+ *  Copyright (c) Elijah Jensen 2015. All Rights Reserved.
+ *  See LICENSE file (MIT).
+ *  ReTFeT/main.c
+ */
+
+
 #include "numc.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,17 +18,17 @@ const double pi = 3.141592;
 
 void computeDFT(double *x, double complex *X, int N)
 {
-int n =0; 
+int n =0;
 double constant = -1*TwoPi/N;
 
-do{ 
-	
-	
+do{
+
+
 	for ( int k =0; k<N/2; k++)
 	{
 		X[k] = X[k] + x[n]*cexp(constant*k*n*I);
 	}
-	
+
 	n++;
 	}while(n<N);
 
@@ -32,39 +39,39 @@ do{
 int main()
 {
 
-	// Define memory 
+	// Define memory
 	int N = 2048;
 	double  *x = zeros(N);
 	double complex *X = zeros(N);
 
 
-//Define the signal here (for testing purposes only) 
+//Define the signal here (for testing purposes only)
 	FILE * fs = fopen("signal.dat","w");
-	
+
 	for ( int i =0; i<N; i++)
 	{
-		x[i] = 0.5*cos(2*pi*i/50.0) + 0.5*sin(2*pi*i/60.0); 
-		// test signal ... two out of phase (90 degree) signals at different frequencies. 
-		
+		x[i] = 0.5*cos(2*pi*i/50.0) + 0.5*sin(2*pi*i/60.0);
+		// test signal ... two out of phase (90 degree) signals at different frequencies.
+
 		fprintf(fs,"%f \n", x[i]); // save to file
-			
+
 	}
 	fclose(fs);
-	
-	
-	computeDFT(x,X,N); // run transform 
-	
-	
+
+
+	computeDFT(x,X,N); // run transform
+
+
 	FILE *fp;
 	fp = fopen("data.dat","w");
-	
-	
-	
+
+
+
 	for ( int i =0; i<N/2; i++)
 	{
 	// compute magnitude and output to file
-	fprintf(fp,"%f \n", sqrt(creal(X[i])*creal(X[i]) + cimag(X[i])*cimag(X[i]))); 
-	
+	fprintf(fp,"%f \n", sqrt(creal(X[i])*creal(X[i]) + cimag(X[i])*cimag(X[i])));
+
 	}
 	fclose(fp);
 
